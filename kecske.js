97a2741d58bfcc8688d9d0aedb2297aa99e20bac
P-Andris <pillerandras0714@gmail.com>
2022-03-30 12:13:53 +0200

    7. feladatig kész

--- kecske.js
+++ kecske.js
@@ -34,6 +34,18 @@ function kurzorKi() {
     event.target.className = "";
 }
 
+function kepCsonakba() {
+    event.target.style.display = "none";
+    /* txt = "<img>";
+    ID("csonak").innerHTML += txt;
+    $("#csonak img").src = event.target.src;
+    $("#csonak img").alt = event.target.alt;
+    event.target.removeEventListener("click", kepCsonakba); */
+    ID("csonak").innerHTML += "<img>";
+    $a("#csonak img")[1].src = event.target.src;
+    $a("#csonak img")[1].alt = event.target.alt;
+}
+
 // MAIN
 function init() {
     $("footer p").innerHTML = "@ Piller András";
@@ -44,6 +56,7 @@ function init() {
     $("#csonak").innerHTML = txt;
     $("#csonak img").src = "kepek/csonak.png";
     $("#csonak img").alt = "csonak";
+    $("#csonak img").className = "csonak";
 
     
     /* console.log($a("#bal img"));
@@ -60,5 +73,9 @@ function init() {
     for(i = 0; i < kepekHossza; i++) {
         $a("#bal img")[i].addEventListener("mouseover", kurzorBe);
         $a("#bal img")[i].addEventListener("mouseout", kurzorKi);
+    }
+
+    for(i = 0; i < kepekHossza; i++) {
+        $a("#bal img")[i].addEventListener("click", kepCsonakba)
     }
 }
