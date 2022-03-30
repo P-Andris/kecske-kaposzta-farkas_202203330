995dcd4f6874bb95d91e13f8036f77d1c2809ae0
P-Andris <pillerandras0714@gmail.com>
2022-03-30 11:42:59 +0200

    6. feladatig kész

---
+++ kecske.js
@@ -0,0 +1,64 @@
+window.addEventListener("load", init);
+
+function ID(e) {
+    return document.getElementById(e);
+}
+
+function CL(e) {
+    return document.getElementsByClassName(e);
+}
+
+function $(e) {
+    return document.querySelector(e);
+}
+
+function $a(e) {
+    return document.querySelectorAll(e);
+}
+
+var kepek = [];
+let i = 0;
+var txt = "";
+
+function kepKatt() {
+    kepek.push(event.target.src);
+    console.log(kepek);
+    event.target.removeEventListener("click", kepKatt);
+}
+
+function kurzorBe() {
+    event.target.className = "kiemel";
+}
+
+function kurzorKi() {
+    event.target.className = "";
+}
+
+// MAIN
+function init() {
+    $("footer p").innerHTML = "@ Piller András";
+    $("footer p").style.textAlign = "center";
+    $("footer p").style.fontSize = "20px";
+
+    txt = txt + "<img>"
+    $("#csonak").innerHTML = txt;
+    $("#csonak img").src = "kepek/csonak.png";
+    $("#csonak img").alt = "csonak";
+
+    
+    /* console.log($a("#bal img"));
+    console.log($a("#bal img")[0]); */
+    var kepekHossza = $a("#bal img").length;
+    for(i = 0; i < kepekHossza; i++) {
+        $a("#bal img")[i].addEventListener("click", kepKatt);
+    }
+    /* kepek.push(event.target.src = "kepek/csonak.png");
+    kepek.push(event.target.src = "kepek/farkas.png");
+    kepek.push(event.target.src = "kepek/kaposzta.png");
+    kepek.push(event.target.src = "kepek/kecske2.png"); */
+
+    for(i = 0; i < kepekHossza; i++) {
+        $a("#bal img")[i].addEventListener("mouseover", kurzorBe);
+        $a("#bal img")[i].addEventListener("mouseout", kurzorKi);
+    }
+}
